f07d3495b74f7f8aa06eac2509eb98f547ed6138
matee <matee@matee.net>
2022-03-13 21:49:52 +0100

    Initial admin models configuration

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import (
+    Goods,
+    Needs,
+    Organization,
+    Poi,
+)
 
-# Register your models here.
+
+class GoodsAdmin(admin.ModelAdmin):
+    pass
+
+
+class NeedsAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+class PoiAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Goods, GoodsAdmin)
+admin.site.register(Needs, NeedsAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Poi, PoiAdmin)
