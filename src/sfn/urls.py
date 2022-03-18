23685c3851e39fde78b83254a648115e524f61ba
matee <matee@matee.net>
2022-03-18 18:56:10 +0100

    Basic home page based on bootstrap

--- src/sfn/urls.py
+++ src/sfn/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oauth/', include('social_django.urls', namespace='social'))
+    path('oauth/', include('social_django.urls', namespace='social')),
+    path('', TemplateView.as_view(template_name="home/index.html"))
 ]
