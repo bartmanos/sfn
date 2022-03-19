b690e85ada16f880c027dc1adfd06c26cd10d450
matee <matee@matee.net>
2022-03-19 19:26:19 +0100

    /terms available #14

--- src/sfn/urls.py
+++ src/sfn/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from core.views import NeedsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('oauth/', include('social_django.urls', namespace='social')),
+    path('terms/', TemplateView.as_view(template_name='terms.html')),
     path('', NeedsView.as_view()),
 ]
