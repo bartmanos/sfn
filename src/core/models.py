7c0331a1315eac863e47268119ee56e5160e5631
ksta <katarzyna.sztuk@webinterpret.com>
2022-03-19 20:02:08 +0100

    shipments cd

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser, Group
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -142,7 +143,14 @@ class Needs(BaseModel):
         verbose_name_plural = _("Needs")
 
     def __str__(self):
-        return f"{self.good.name} - {self.quantity} {self.unit} - {_('Needs.due_time')}: {self.due_time}"
+        return (
+            f"{self. status} > {self.good.name} - {self.quantity} {self.unit} - {_('Needs.due_time')}: {self.due_time}"
+        )
+
+    def save(self, *args, **kwargs):
+        if self.status == Needs.Status.FULFILLED:
+            self.shipment.update(status=Shipments.Status.DONE)
+        return super().save(*args, **kwargs)
 
 
 class Shipments(BaseModel):
@@ -155,9 +163,20 @@ class Shipments(BaseModel):
     status = models.CharField(_("Shipments.status"), choices=Status.choices, max_length=32)
 
     def save(self, *args, **kwargs):
-        self.need.status = Needs.Status.FULFILLED if self.status == self.Status.DONE else Needs.Status.DISABLED
+        self.need.status = Needs.Status.DISABLED
         self.need.save()
         return super().save(*args, **kwargs)
+
+    def clean(self):
+        if (
+            Shipments.objects.filter(
+                status__in=(Shipments.Status.TO_DO, Shipments.Status.IN_PROGRESS),
+                created_by=self.created_by,
+            ).count()
+            >= 20
+        ):
+
+            raise ValidationError(_("Too many shipments for one user"))
 
     class Meta:
         verbose_name = _("Shipments")
