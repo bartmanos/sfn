4297320be553dbcef84cac32773d192fd6cd424e
matee <matee@matee.net>
2022-03-20 18:36:11 +0100

    #52 POI details accessible

--- src/core/views.py
+++ src/core/views.py
@@ -1,23 +1,23 @@
-from django.http import HttpResponseForbidden
-from django.views.generic import ListView
-
-from core.models import Needs, Shipments
 from django.core.exceptions import ValidationError
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
+from core.models import Needs, Poi, Shipments
 
 
 def need_endpoint(request):
-    context = {'needs': Needs.objects.filter(status=Needs.Status.ACTIVE).order_by("-created_at")}
+    context = {"needs": Needs.objects.filter(status=Needs.Status.ACTIVE).order_by("-created_at")}
     if request.method == "POST":
         try:
-            need_id = request.GET.get('need_id')
+            need_id = request.GET.get("need_id")
             Shipments.objects.create(
                 need_id=need_id,
                 status=Shipments.Status.IN_PROGRESS,
                 created_by=request.user,
             )
         except ValidationError as e:
-            context['errors'] = dict(e).values()
+            context["errors"] = dict(e).values()
 
     return render(
         request=request,
@@ -40,3 +40,7 @@ class MyShipmentsView(ListView):
             "-created_at"
         )
         return realized.union(unrealized)
+
+
+class PoiView(DetailView):
+    model = Poi
